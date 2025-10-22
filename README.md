83032f4143e5229226aec671cb37976e87fc0b53
pc942 <piyushchdry01@gmail.com>
2025-10-21 22:58:27 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cxx-to-rust-agent
+Autonomous agents that migrate C++ to Rust with verification.
---
+++ migrator/logging_setup.py
@@ -0,0 +1,4 @@
+import logging
+from rich.logging import RichHandler
+logging.basicConfig(level=logging.INFO, format="%(message)s", handlers=[RichHandler(rich_tracebacks=True)])
+logger = logging.getLogger("migrator")
